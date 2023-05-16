833367fed9cbdbf3135325f6ab29e08123b8a01a
SMR <mohammad.rouzegar@gmail.com>
2023-05-16 16:22:54 +0330

    Strater project created

---
+++ config.py
@@ -0,0 +1,13 @@
+class Api(object):
+    API_TITLE = "Ad system - REST API"
+    API_VERSION = "v1"
+
+    OPENAPI_VERSION = "3.0.3"
+    OPENAPI_URL_PREFIX = "/"
+
+    OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
+    OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.25.x/"
+
+
+class Config(Api):
+    pass
